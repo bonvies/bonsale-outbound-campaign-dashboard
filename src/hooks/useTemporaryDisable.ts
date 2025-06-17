6833a47af6e7fe276748f87e5a1800f8e7619760
Aruminiya <leo80103@gmail.com>
2025-06-17 10:27:36 +0800

    feat: Code Optimization

--- src/hooks/useTemporaryDisable.ts
+++ src/hooks/useTemporaryDisable.ts
@@ -1,11 +1,11 @@
 import { useState, useCallback, useRef, useEffect } from 'react';
 
-export default function useTemporaryDisable<T extends string | number | symbol = string>(timeout = 1000) {
+export default function useTemporaryDisable<T extends string | number | symbol = string>(defaultTimeout = 1000) {
   const [disabledMap, setDisabledMap] = useState<Record<T, boolean>>({} as Record<T, boolean>);
   const timerRef = useRef<Record<T, number>>({} as Record<T, number>);
   const timers = timerRef.current;
 
-  const triggerDisable = useCallback((key: T) => {
+  const triggerDisable = useCallback((key: T, timeout?: number) => {
     setDisabledMap(prev => ({ ...prev, [key]: true }));
     if (timerRef.current[key]) {
       clearTimeout(timerRef.current[key]);
@@ -13,8 +13,8 @@ export default function useTemporaryDisable<T extends string | number | symbol =
     timerRef.current[key] = window.setTimeout(() => {
       setDisabledMap(prev => ({ ...prev, [key]: false }));
       delete timerRef.current[key];
-    }, timeout);
-  }, [timeout]);
+    }, timeout ?? defaultTimeout);
+  }, [defaultTimeout]);
 
   useEffect(() => {
     return () => {
